bab9e4e4d2dae3f873299c645eaed9e7910e1813
AlexandraaLupu <alexandra.lupu@stud.ubbcluj.ro>
2024-03-19 17:10:23 +0200

    Add project

---
+++ src/test/java/service/ServiceTest.java
@@ -0,0 +1,14 @@
+package service;
+import static org.junit.Assert.*;
+
+import domain.Student;
+import org.junit.Test;
+
+public class ServiceTest {
+
+    @Test
+    public void testAddStudentEmptyId() {
+        //Student student = new Student("", "Alexandra", 934, "a@a.com");
+        assertEquals(1,1);
+    }
+}
